7bf5c06d9f96cd3942e20ed85963fc275dbf454d
xenonbg <anton.atanasov86@gmail.com>
2015-09-03 18:26:26 +0300

    Added first problem source code

--- JavaBasicsExam3September/src/DozenEggs.java
+++ JavaBasicsExam3September/src/DozenEggs.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class DozenEggs {
     public static void main(String[] args) {
+        int finalDozens = 0;
+        int finalEggs = 0;
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 0; i < 7; i++) {
+            String[] currentLineInput = scanner.nextLine().split(" ");
+            String currentMeasure = currentLineInput[1];
+            int currentNumber = Integer.parseInt(currentLineInput[0]);
+            if (currentMeasure.equals("eggs")) {
+                finalEggs += currentNumber;
+            } else {
+                finalDozens += currentNumber;
+            }
+        }
+
+        while (finalEggs >= 12) {
+            int dozens = finalEggs / 12;
+            finalEggs%=12;
+            finalDozens+= dozens;
+        }
+
+        System.out.printf("%1$d dozens + %2$d eggs",finalDozens,finalEggs);
     }
 }
